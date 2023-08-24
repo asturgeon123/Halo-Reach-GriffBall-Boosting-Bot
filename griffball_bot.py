1d2a501bb8092c69ba46d27ba7818d23d1cf77e2
Alex <32243510+asturgeon123@users.noreply.github.com>
2023-08-24 18:11:24 -0500

    Update griffball_bot.py

--- griffball_bot.py
+++ griffball_bot.py
@@ -55,28 +55,27 @@ def print_title():
 	
 	"""
 
-	author = """	An autonomous farming bot that plays griffball in Halo Reach
-	______________________________
-	|                            |
-	| Created by: @Alex Sturgeon | 
-	|____________________________|
+	author = """An autonomous farming bot that plays griffball in Halo Reach
+______________________________
+|                            |
+| Created by: @Alex Sturgeon | 
+|____________________________|
 
-	Calibration: A window will pop up with a screen shot of your monitor. Just follow the prompts.
+Calibration: A window will pop up with a screen shot of your monitor. Just follow the prompts.
 
-	Controls:
-		- Press 'ctrl + c' to quit
-		- To Restart Calibration, delete the capture_config.json file and restart the bot
+Controls:
+	- Press 'ctrl + c' to quit
+	- To Restart Calibration, delete the capture_config.json file and restart the bot
 
-	Note: This bot is a visual bot, it does not read memory or anything like that. It just looks at the screen and clicks.
-	      If you move you window around or change the resolution, you will need to recalibrate the bot.
+Note: This bot is a visual bot, it does not read memory or anything like that. It just looks at the screen and clicks.
+	  If you move you window around or change the resolution, you will need to recalibrate the bot.
 
 -----------------------------------------------------------------------------
 	"""
 
-	gradient_print(logo_text, 
-		start_color=0x4BBEE3, 
-		end_color=Color.medium_violet_red
-	)
+	#gradient_print(logo_text, start_color=0x4BBEE3, end_color=Color.medium_violet_red)
+
+	rgbprint(logo_text, color=Color.medium_violet_red)
 
 	rgbprint(author, color=Color.medium_violet_red)
 
@@ -100,7 +99,8 @@ def main():
 	#print("Center Value", capture.center)
 	if capture.center is None:
 		rgbprint('[!] Calibration Needed.',color=Color.medium_violet_red)
-		rgbprint(' -  Click into the game when a minimap is visible and calibration will start automatically.',color=Color.medium_violet_red)
+		rgbprint(' -  Click into the game when a minimap is visible and calibration will start automatically. NOTE: Two Screens will popup on after the other...',color=Color.medium_violet_red)
+		
 		time.sleep(3)
 		while True:
 			if GetWindowText(GetForegroundWindow()) == "Halo: The Master Chief Collection  ":
@@ -123,79 +123,80 @@ def main():
 		if GetWindowText(GetForegroundWindow()) != "Halo: The Master Chief Collection  ":
 			rgbprint('[!] MCC Window not in focus. Pausing...',color=Color.medium_violet_red)
 			time.sleep(3)
-				
-		frame, grey = capture.get_frame()
-		center = (int(frame.shape[0]/2), int(frame.shape[1]/2))
-
-		if any(frame[center[0]][center[1]]) == 0:
-			rgbprint('Waiting for game',color=Color.medium_violet_red)
-			#print('Waiting for game')
-			time.sleep(5)
 
 		else:
-			contours, _ = cv2.findContours(
-				grey, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+			frame, grey = capture.get_frame()
+			center = (int(frame.shape[0]/2), int(frame.shape[1]/2))
 
-			# Choose the contour closest to the center
-			w, h = 0, 0  # width and height of a blob
-			shortx, shorty, shortw, shorth = 0, 0, 0, 0  # values of the closest blob
-			shortest_distance = 5000
-			for c in contours:
-				x, y, w, h = cv2.boundingRect(c)
+			if any(frame[center[0]][center[1]]) == 0:
+				rgbprint('Waiting for game',color=Color.medium_violet_red)
+				#print('Waiting for game')
+				time.sleep(5)
 
-				distance = calcdistance(x, y, center[0], center[1])
-				if distance < shortest_distance:
-					shortest_distance = distance
-					shortx, shorty, shortw, shorth = (x, y, w, h)
-				else:
-					cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-				cv2.putText(frame, '. Distance: ' + str(distance), (x, y),
-							cv2.FONT_HERSHEY_COMPLEX_SMALL, .5, (255, 255, 255))
-
-			# Little extra drawng in our cv2 window
-			cv2.rectangle(frame, (shortx, shorty),
-						  (shortx+shortw, shorty+shorth), (0, 255, 255), 2)
-			cv2.line(frame, (center[0], center[1]+500),
-					 (center[0], center[1]-500), (255, 0, 0))
-			cv2.line(frame, (center[0]+500, center[1]),
-					 (center[0]-500, center[1]), (255, 0, 0))
-			cv2.line(frame, (center[0]+500, center[1]+xlevel),
-					 (center[0]-500, center[1]+xlevel), (0, 0, 255))
-
-			# angle of the closest dot
-			angle = calcAngle(shortx, shorty, center[0], center[1])
-
-			# movement and look around commands
-			if len(contours) < 1:
-				PressKey(W)
-				ReleaseKey(A)
-				ReleaseKey(D)
-				ReleaseKey(S)
 			else:
-				if shortx+w/2 > center[0]:
-					ReleaseKey(A)
-					PressKey(D)
-				elif shortx+w/2 < center[0]:
-					ReleaseKey(D)
-					PressKey(A)
-				if shorty+h/2 > center[1] + xlevel:
-					ReleaseKey(W)
-					PressKey(S)
-				elif shorty+h/2 < center[1] + xlevel:
-					ReleaseKey(S)
+				contours, _ = cv2.findContours(
+					grey, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+				# Choose the contour closest to the center
+				w, h = 0, 0  # width and height of a blob
+				shortx, shorty, shortw, shorth = 0, 0, 0, 0  # values of the closest blob
+				shortest_distance = 5000
+				for c in contours:
+					x, y, w, h = cv2.boundingRect(c)
+
+					distance = calcdistance(x, y, center[0], center[1])
+					if distance < shortest_distance:
+						shortest_distance = distance
+						shortx, shorty, shortw, shorth = (x, y, w, h)
+					else:
+						cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+					cv2.putText(frame, '. Distance: ' + str(distance), (x, y),
+								cv2.FONT_HERSHEY_COMPLEX_SMALL, .5, (255, 255, 255))
+
+				# Little extra drawng in our cv2 window
+				cv2.rectangle(frame, (shortx, shorty),
+							(shortx+shortw, shorty+shorth), (0, 255, 255), 2)
+				cv2.line(frame, (center[0], center[1]+500),
+						(center[0], center[1]-500), (255, 0, 0))
+				cv2.line(frame, (center[0]+500, center[1]),
+						(center[0]-500, center[1]), (255, 0, 0))
+				cv2.line(frame, (center[0]+500, center[1]+xlevel),
+						(center[0]-500, center[1]+xlevel), (0, 0, 255))
+
+				# angle of the closest dot
+				angle = calcAngle(shortx, shorty, center[0], center[1])
+
+				# movement and look around commands
+				if len(contours) < 1:
 					PressKey(W)
-				moveMouse(x=2*angle, y=0)
+					ReleaseKey(A)
+					ReleaseKey(D)
+					ReleaseKey(S)
+				else:
+					if shortx+w/2 > center[0]:
+						ReleaseKey(A)
+						PressKey(D)
+					elif shortx+w/2 < center[0]:
+						ReleaseKey(D)
+						PressKey(A)
+					if shorty+h/2 > center[1] + xlevel:
+						ReleaseKey(W)
+						PressKey(S)
+					elif shorty+h/2 < center[1] + xlevel:
+						ReleaseKey(S)
+						PressKey(W)
+					moveMouse(x=2*angle, y=0)
 
-			# strike command
-			if shortest_distance < attack_distance and np.absolute(angle) < 10:
-				clickMouse()
+				# strike command
+				if shortest_distance < attack_distance and np.absolute(angle) < 10:
+					clickMouse()
 
-			# display the cv2 frame
-			if show == "True":
-				cv2.imshow("Halo_Reach_Griff_Bot", frame)
-			# exit situations
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				break
+				# display the cv2 frame
+				if show == "True":
+					cv2.imshow("Halo_Reach_Griff_Bot", frame)
+				# exit situations
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+					break
 
 
 if __name__ == "__main__":
